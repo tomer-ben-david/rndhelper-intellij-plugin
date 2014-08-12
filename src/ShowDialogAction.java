e0cc1b5da0a8a6e71142944e80d2993e000d7141
tomerbd1 <tomerbd1@gmail.com>
2014-08-12 08:05:32 +0300

    Initial commit

---
+++ src/ShowDialogAction.java
@@ -0,0 +1,12 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @author tomerb
+ *         on 12/08/14.
+ */
+public class ShowDialogAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
