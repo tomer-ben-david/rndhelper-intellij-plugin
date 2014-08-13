98eeb928c95983ed088be5fabcb0e2e07c005048
tomerbd1 <tomerbd1@gmail.com>
2014-08-13 08:49:03 +0300

    added show dialog option according to: http://confluence.jetbrains.com/display/IDEADEV/Getting+Started+with+Plugin+Development

--- src/ShowDialogAction.java
+++ src/ShowDialogAction.java
@@ -1,12 +1,18 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 
 /**
  * @author tomerb
- *         on 12/08/14.
+ * on 12/08/14.
  */
 public class ShowDialogAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
         // TODO: insert action logic here
+        Project project = e.getData(PlatformDataKeys.PROJECT);
+        String txt= Messages.showInputDialog(project, "What is your name?", "Input Your Name", Messages.getQuestionIcon());
+        Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
     }
 }
